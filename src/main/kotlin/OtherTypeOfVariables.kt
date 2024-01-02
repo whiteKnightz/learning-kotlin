555929284508af2af4e9f6ec30df71ebdba968c6
Abhishek Satyal <ab.satyal@gmail.com>
2024-01-02 07:58:17 -0500

    feat: testing kotlin

---
+++ src/main/kotlin/OtherTypeOfVariables.kt
@@ -0,0 +1,27 @@
+import java.math.BigDecimal
+import kotlin.math.roundToInt
+
+fun main() {
+    val dbl = 24.3
+    print("Is dbl a double: ${dbl is Double}")
+    print("\nIs dbl a double: ${dbl::class.qualifiedName}")
+    print("\ndbl's java class is: ${dbl.javaClass}")
+    val myInt = dbl.roundToInt()
+    print("\nWhat is myInt: ${myInt::class.qualifiedName}")
+
+    val anotherInt: Int = 16
+    val myFloat: Float = 1.1f
+    print("\nWhat is myFloat: ${myFloat::class.qualifiedName}")
+
+    val result: Float = myFloat+anotherInt
+    print("\nWhat is result: ${result::class.qualifiedName}")
+    print("\nWhat is result: ${result}")
+
+    val myBigD = BigDecimal(17)
+    print("\nWhat is myBigD: ${myBigD::class.qualifiedName}")
+    val myBigD2: BigDecimal
+    myBigD2 = myBigD.add(BigDecimal(21))
+    print("\nWhat is myBigD2: ${myBigD2}")
+
+    print("\n")
+}
---
+++ src/main/kotlin/StringsVariables.kt
@@ -0,0 +1,48 @@
+var name: String? = null
+
+fun main() {
+    printNameWithIf()
+    printNameWithWhile()
+    println("\n\n")
+
+    name = "Abhishek"
+    printNameWithIf()
+    printNameWithWhile()
+    println("\n\n")
+
+    name = "Abhishek Satyal"
+    printNameWithIf()
+    printNameWithWhile()
+    println("\n\n")
+
+    name = "Vijay"
+    printNameWithIf()
+    printNameWithWhile()
+    println("\n\n")
+
+    modifyName("Vijay Kr. Sharma")
+    printNameWithIf()
+    printNameWithWhile()
+    println("\n\n")
+}
+
+private fun printNameWithIf() {
+    if (name != null) {
+        println(name)
+    } else{
+        println("No name!")
+    }
+}
+
+private fun printNameWithWhile() {
+    when(name){
+        null -> println("Name is null")
+        "Abhishek" -> println("Name is Abhishek!")
+        "Abhishek Satyal" -> println("Name is Abhishek Satyal!")
+        else -> println("Name is: $name")
+    }
+}
+
+private fun modifyName(nameVal: String){
+    name=nameVal
+}
